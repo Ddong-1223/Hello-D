9fa807d8c73143b3361355846bf342d92f1eae42
Dong <1917806299@qq.com>
2020-11-09 22:49:06 +0800

    day3

---
+++ day3.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	//换行符  \n
+
+	cout << "hello world\n" << endl;
+	//反斜杠  \\
+
+	cout << "\\" << endl;
+
+	//水平制表符 \t
+
+	cout << "hello\tworld" << endl;//hello+空位共八格
+	cout << "he\tworld" << endl;
+
+	system("pause");
+	return 0;
+}
