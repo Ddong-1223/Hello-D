94e087e5263804b0f8157d492586a07065649cd5
Dong <1917806299@qq.com>
2020-11-10 23:12:51 +0800

    day4

---
+++ day4.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+	char str1[] = "hello world";//c语言风格
+	cout << str1 << endl;
+
+	string str2 = "hello world";//c++风格
+	cout << str2 << endl;
+
+	system("pause");
+	return 0;
+}
