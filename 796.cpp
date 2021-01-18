ad0a071552045c90085f2d84d3cafa01b92ae7d8
Dong <ddong86800@163.com>
2021-01-18 13:53:20 +0800

    二维前缀和

---
+++ 796.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdio>
+
+using namespace std;
+
+const int N=1e3+10;
+int a[N][N],S[N][N];
+
+int main(){
+    int n,m,q;
+    scanf("%d %d %d",&n,&m,&q);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=m;j++){
+            scanf("%d",&a[i][j]);
+            S[i][j]=S[i-1][j]+S[i][j-1]-S[i-1][j-1]+a[i][j];
+        }
+    }
+    while(q--){
+        int x1,y1,x2,y2;
+        scanf("%d %d %d %d",&x1,&y1,&x2,&y2);
+        printf("%d\n",S[x2][y2]-S[x1-1][y2]-S[x2][y1-1]+S[x1-1][y1-1]);
+    }
+    return 0;
+}
